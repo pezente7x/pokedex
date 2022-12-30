b6974550e58758ef273bcdf401a1c47338e701fc
Carlos <carlos.pezente@gmail.com>
2022-12-30 14:31:08 -0300

    segunda versao

---
+++ script.js
@@ -0,0 +1,54 @@
+var formulario = document.querySelector("form");
+
+formulario.addEventListener("submit", function (e) {
+  // Bloqueia o refresh da página
+  e.preventDefault();
+
+  // Url da pesquisa
+  let urlForm = "https://pokeapi.co/api/v2/pokemon/";
+
+  // Valor do inpt Name
+  let nome = document.getElementById("name");
+
+  // Concatena a url com o inputname
+  urlForm = urlForm + this.name.value;
+
+  // Transforma os valores em minúsculas
+  urlForm = urlForm.toLocaleLowerCase();
+
+  // ID Content
+  let resposta = document.getElementById("content");
+
+  // Id ImgPokemon
+  let imagem = document.getElementById("imgPokemon");
+
+  // Resposta em HTML
+  let html = "";
+
+  fetch(urlForm)
+    .then((resposta) => resposta.json())
+    .then(function (data) {
+      html = "Nome: " + maiuscula(data.name) + "<br>";
+      html = html + "Type: " + maiuscula(data.types[0].type.name);
+      resposta.innerHTML = html;
+
+      imagem.innerHTML =
+        "<img src='" +
+        data.sprites.front_default +
+        "'><img src='" +
+        data.sprites.back_default +
+        "'>";
+    })
+    .catch(function (err) {
+      if (err == "Pokémon não encontrado!") {
+        html = "Pokémon não encontrado! 😒";
+      } else {
+        html = err;
+      }
+      resposta.innerHTML = html;
+    });
+});
+
+function maiuscula(val) {
+  return val[0].toUpperCase() + val.substr(1);
+}
